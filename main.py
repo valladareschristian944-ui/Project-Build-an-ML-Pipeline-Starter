192146170b1ee9064ed24842dc8216f2be2767cb
Christian Valladares <valladareschristian944@gmail.com>
2026-08-14 17:19:59 -0400

    Implement test_regression_model step

--- main.py
+++ main.py
@@ -113,13 +113,15 @@ def go(config: DictConfig):
                 },
             )
 
-        if "test_regression_model" in active_steps:
-
-            ##################
-            # Implement here #
-            ##################
-
-            pass
+        _ = mlflow.run(
+                f"{config['main']['components_repository']}/test_regression_model",
+                "main",
+                env_manager="conda",
+                parameters={
+                    "mlflow_model": "model_export:prod",
+                    "test_dataset": "test_data.csv:latest",
+                },
+            )
 
 
 if __name__ == "__main__":
